77f7a9af142085fce704355555b3e4f39a78e7b7
tochman <thomas@nocebo.se>
2015-09-09 14:23:33 +0200

    demo code

---
+++ lib/my_hash.rb
@@ -0,0 +1,70 @@
+require 'terminal-table'
+require 'byebug'
+
+class MyHash
+  attr_accessor :grid
+  
+  def initialize
+    self.grid = populate_grid
+  end
+  
+  def check_neighbours(coord)
+    if all_water_cells?(coord)
+      :all_water
+    else
+      :ships_around
+    end
+  end
+  
+  
+  def populate_grid
+    grid = {}
+    [*'A'..'J'].each do |l|
+      [*1..10].each do |n|
+       grid["#{l}#{n}".to_sym] = 'w'
+      end
+    end
+    grid
+  end
+  
+  def draw_grid
+    rows = get_grid_values
+    table = Terminal::Table.new :headings => ['', *'1'..'10'], :rows => rows
+    table.render
+  end
+  
+  def get_grid_values
+    grid_values = []
+    [*'A'..'J'].each do |letter|
+      row_hash = self.grid.select { |key, value| key.to_s.match(letter) }
+      arr = ["#{letter}"]
+      arr.push(row_hash.values)
+      grid_values.push(arr.flatten)
+    end
+    grid_values
+  end  
+  
+  def place_ship(coord)
+    if self.grid[coord] == 'w'
+      self.grid[coord] = 's'
+    else
+      'can not do that'
+    end
+  end
+  
+  def all_water_cells?(coord)
+    right_n = get_right_n_coord(coord)
+    left_n = get_left_n_coord(coord)
+    self.grid[right_n] == 'w' && self.grid[left_n] == 'w'
+  end
+  
+  def get_right_n_coord(coord)
+    coord.next.to_sym
+  end
+  
+  def get_left_n_coord(coord)
+    l = (coord[0].codepoints.first - 1).chr 
+    coord.to_s.gsub(coord[0],l).to_sym
+  end
+  
+end
---
+++ spec/my_hash_spec.rb
@@ -0,0 +1,72 @@
+require 'my_hash'
+require 'byebug'
+
+describe MyHash do
+  
+  subject { MyHash.new }
+  
+  it {is_expected.to respond_to :grid}
+  it {is_expected.to respond_to :grid=}
+  it {is_expected.to respond_to :populate_grid}
+  it {is_expected.to respond_to :check_neighbours}
+  it {is_expected.to respond_to :draw_grid}
+  
+  it 'grid is a hash' do
+    expect(subject.grid).to be_kind_of Hash
+  end
+  
+  it 'grid is populated with coodinate keys' do
+    [*:A..:J].each do |k|
+      [*1..10].each do |v|
+        key = [k, v].join('')
+        expect(subject.grid).to include key.to_sym
+      end
+    end
+  end
+  
+  
+  it 'grid coodinate keys are set to water' do
+    [*:A..:J].each do |k|
+      [*1..10].each do |v|
+        key = [k, v].join('')
+        expect(subject.grid[key.to_sym]).to eq 'w'
+      end
+    end
+  end
+  
+  it 'checks for ships in neighbour cells' do
+    expect(subject.check_neighbours(:B1)).to eq :all_water
+  end
+  
+  it 'draws a table' do
+
+    actual_table = "
+    +---+---+---+---+---+---+---+---+---+---+----+
+    |   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 |
+    +---+---+---+---+---+---+---+---+---+---+----+
+    | A | w | w | w | w | w | w | w | w | w | w  |
+    | B | w | w | w | w | w | w | w | w | w | w  |
+    | C | w | w | w | w | w | w | w | w | w | w  |
+    | D | w | w | w | w | w | w | w | w | w | w  |
+    | E | w | w | w | w | w | w | w | w | w | w  |
+    | F | w | w | w | w | w | w | w | w | w | w  |
+    | G | w | w | w | w | w | w | w | w | w | w  |
+    | H | w | w | w | w | w | w | w | w | w | w  |
+    | I | w | w | w | w | w | w | w | w | w | w  |
+    | J | w | w | w | w | w | w | w | w | w | w  |
+    +---+---+---+---+---+---+---+---+---+---+----+
+    "
+    
+    expect(subject.draw_grid).to match actual_table
+  end
+  
+  it 'places a ship on specific coordinate' do
+    subject.place_ship(:G1)
+    expect(subject.grid[:G1]).to eq 's' 
+  end
+  
+  it 'does not plac a ship on specific coordinate if not water' do
+    subject.grid[:E1] = 's'
+    expect(subject.place_ship(:E1)).to eq 'can not do that' 
+  end
+end
